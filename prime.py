8cb4121cfd28c50ad42e69a66b5a3a15f8a78bd9
HtetWYan <89427803+HtetWYan@users.noreply.github.com>
2022-10-14 21:17:00 -0400

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,9 @@
+print(2)
+print(3)
+for i in range(1,100):
+    x = (6 * i)- 1
+    y = (6 * i) + 1
+    if x % 5 != 0 and x % 7 != 0:
+        print(x)
+    if y % 5 != 0 and y % 7 != 0:
+        print(y)
